9948134fc9c99cc68b008b6a756572bc4e0582ce
AionWilliam <willizeeeeee@gmail.com>
2023-10-16 14:56:03 -0400

    [Req2] created class list and change class color when minimum req is met

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import { useState } from "react";
 import "./App.css";
 import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from "./consts.js";
 
+const CONTAINER_STYLE = {
+  margin: "10px",
+  padding: "10px",
+  border: "2px solid white",
+  borderRadius: "5px",
+};
+
 function App() {
   const initialAttributesState = ATTRIBUTE_LIST.reduce((state, attribute) => {
-    state[attribute] = 0;
+    state[attribute] = 10;
     return state;
   }, {});
 
@@ -26,19 +33,20 @@ function App() {
     }
   };
 
+  const isClassRequirementsMet = (className) => {
+    const classRequirements = CLASS_LIST[className];
+    return ATTRIBUTE_LIST.every(
+      (attribute) => attributes[attribute] >= classRequirements[attribute]
+    );
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Coding Exercise</h1>
       </header>
-      <section className="App-section">
-        <div
-          style={{
-            margin: "10px",
-            border: "2px solid white",
-            borderRadius: "5px",
-          }}
-        >
+      <section className="App-section" style={{ display: "flex" }}>
+        <div style={CONTAINER_STYLE}>
           <h2>Character Attributes</h2>
           <ul style={{ listStyleType: "none", padding: 0 }}>
             {ATTRIBUTE_LIST.map((attribute) => (
@@ -47,6 +55,21 @@ function App() {
                 <button onClick={() => decrementAttribute(attribute)}>-</button>
                 <span>{attributes[attribute]}</span>
                 <button onClick={() => incrementAttribute(attribute)}>+</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div style={CONTAINER_STYLE}>
+          <h2>Character Classes</h2>
+          <ul style={{ listStyleType: "none", padding: 0 }}>
+            {Object.keys(CLASS_LIST).map((className) => (
+              <li
+                key={className}
+                style={{
+                  color: isClassRequirementsMet(className) && "green",
+                }}
+              >
+                {className}
               </li>
             ))}
           </ul>
